87cf062787170368fb4ca374e644079199575718
Hiba-Almade <hibaalmade998@gmail.com>
2021-11-03 23:08:58 +0200

    add feed

--- src/main/java/com/example/codefellowship/repositories/PostRepository.java
+++ src/main/java/com/example/codefellowship/repositories/PostRepository.java
@@ -1,7 +1,9 @@
 package com.example.codefellowship.repositories;
 
+import com.example.codefellowship.models.ApplicationUser;
 import com.example.codefellowship.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post,Integer> {
+    Post[] findByUsersPost(ApplicationUser applicationUser);
 }
